01ba5a6676d126f972d97cfa1182b0ca27c85a64
nirvanasupermind <nirvana.supermind@gmail.com>
2021-07-01 00:58:57 -0700

    added node classes

--- skink.py
+++ skink.py
@@ -180,6 +180,27 @@ class Lexer:
 
 
 #######################################
+# NODES
+#######################################
+class NumberNode:
+    def __init__(self, tok):
+        self.tok = tok
+    
+    def __repr__(self):
+        return f'{self.tok}'
+
+class BinOpNode:
+    def __init__(self, left_node, op_tok, right_node):
+        self.left_node = left_node
+        self.op_tok = op_tok
+        self.right_node = right_node
+    
+    def __repr__(self):
+        return f'({self.left_node}, {self.op_tok}, {self.right_node})'
+
+
+
+#######################################
 # RUN
 #######################################
 
