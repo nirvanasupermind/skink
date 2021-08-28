d660384de384e7257b2cb7f70d9f754b84576856
nirvanasupermind <nirvana.supermind@gmail.com>
2021-08-28 02:00:38 -0700

    compile

--- Test.java
+++ Test.java
@@ -4,8 +4,7 @@ public class Test {
     public static void main(String[] args) {
         Lexer lexer = new Lexer("text");
         lexer.advance();
-        lexer.advance();
-        
-        System.out.println(lexer.currentChar);
+
+        System.out.println(lexer.currentChar); //e
     }
 }
