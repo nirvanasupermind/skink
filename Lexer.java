77ce5fd5fc10658cdbc33c62459582d176e942a6
nirvanasupermind <nirvana.supermind@gmail.com>
2021-09-20 00:38:31 -0700

    check in

--- Lexer.java
+++ Lexer.java
@@ -33,8 +33,10 @@ public class Lexer {
             if(Constants.WHITESPACE.indexOf(c) != -1) {
                 
             } else if(Constants.NEWLINES.indexOf(c) != -1) {
-                // tokens.add(new Token(line, TokenType.NEWLINE, String.valueOf(c)));
+                tokens.add(new Token(line, TokenType.NEWLINE, String.valueOf(c)));
                 line++;
+            } else if(c == ';') {
+                tokens.add(new Token(line, TokenType.SEMICOLON, String.valueOf(c)));
             } else if(c == '(') {
                 tokens.add(new Token(line, TokenType.OPEN_BRACE, String.valueOf(c)));
             } else if(c == ')') {
