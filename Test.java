c7a2480803ed59bbbba6081772c4b80e376f406a
nirvanasupermind <nirvana.supermind@gmail.com>
2021-09-20 01:11:49 -0700

    change example file

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ package com.github.skink;
 
 public class Test {
     public static void main(String[] args) {
-        String source = "1 + ";
+        String source = "1 + 2 * 3";
 
         Skink.runString(source);
     }
